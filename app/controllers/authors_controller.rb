ddefc89f7d6863a6083ef00d51eff7a0e3f059b6
Elle Faraday <faraday.elle@gmail.com>
2018-06-09 21:03:03 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -21,20 +21,6 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def update
-    @author = Author.find(params[:id])
-    @author.name = author_params(:name)
-    @author.email = author_params(:email)
-    @author.phone_number = author_params(:phone_number)
-
-    if @author.valid?
-      @author.save
-      redirect_to author_path(@author)
-    else
-      render :edit
-    end
-  end
-
   private
 
   def author_params
